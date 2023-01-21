4c8be1748b2e4d1e83d10520e5ef17c484e0c397
xzebcex <123227926+xzebcex@users.noreply.github.com>
2023-01-20 21:56:02 -0800

    Create greet.py

---
+++ greet.py
@@ -0,0 +1,11 @@
+# This Program greet's the user with hello and ask for our name.
+
+# This Program greet's with hello and ask for our name.
+
+print("Hello!")
+
+name = input("What is your name > ")  # ask for our name.
+print("It's lovely meeting you, " + (name)+'!')
+
+age = input("How old are you "+(name)+'?\n')  # ask for our age.
+print("You will be " + str(int(age) + 1)+" in a year.")
