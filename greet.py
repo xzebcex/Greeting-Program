9488d1877fa38276e0e4357e8df00ba7394cc1c6
Aman Pandit <amanpandit3027@gmail.com>
2023-02-06 06:14:05 +0530

    Update greet.py
    made change using formated string on line 6,8,9

--- greet.py
+++ greet.py
@@ -3,7 +3,7 @@
 print("Hello!")
 
 name = input("What is your name > ")  # ask for our name.
-print("It's lovely meeting you, " + (name)+'!')
+print(f"It's lovely meeting you,  {name}!")
 
-age = input("How old are you "+(name)+'?\n')  # ask for our age.
-print("You will be " + str(int(age) + 1)+" in a year.")
+age = input(f"How old are you {name}?\n")  # ask for our age.
+print(f"You will be {str(int(age) + 1)} in a year.")
