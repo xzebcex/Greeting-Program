1b9f6df1627d3acf44b9901595c7a9cc9b16b993
Aman Pandit <amanpandit3027@gmail.com>
2023-02-06 06:15:55 +0530

    Update greet.py

--- greet.py
+++ greet.py
@@ -6,4 +6,4 @@ name = input("What is your name > ")  # ask for our name.
 print(f"It's lovely meeting you,  {name}!")
 
 age = input(f"How old are you {name}?\n")  # ask for our age.
-print(f"You will be {str(int(age) + 1)} in a year.")
+print(f"You will be {str(int(age) + 1)} in a year.{name}")
